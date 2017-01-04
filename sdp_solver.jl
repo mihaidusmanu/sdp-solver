3390aded3201890c0f37e6cb82e7ccdfa77cb0f3
Mihai Dusmanu <mihai.dusmanu@yahoo.com>
2017-01-04 20:34:11 +0100

    Fixed a bug in the nabla^2. Added random generators.

--- sdp_solver.jl
+++ sdp_solver.jl
@@ -1,17 +1,7 @@
-eps = 1e-6;
-C = [[1 2 3]; [2 9 0]; [3 0 7]];
-A = zeros(2, 3, 3);
-A[1, :, :] = [[1, 0, 1]; [0, 3, 7]; [1, 7, 5]];
-A[2, :, :] = [[0, 2, 8]; [2, 6, 0]; [8, 0, 4]];
-b = [11; 19];
-# opt value 13.9
-#C = [[1 0 0 0]; [0 1 0 0]; [0 0 2 0];[0 0 0 2]];
-#A = zeros(1, 4, 4);
-#A[1, :, :] = [[1 0 0 0]; [0 1 0 0]; [0 0 0 0]; [0 0 0 0]];
-#b = [4];
-# opt value 4
+eps = 1e-6
 
 function sdp_solver(C, A, b)
+  # Find a stricly feasible point
   m = size(A, 1)
   n = size(A, 2)
   eye3d = zeros(1, n, n)
@@ -23,10 +13,12 @@ function sdp_solver(C, A, b)
   if mu[m + 1] >= 0
     error("No strictly feasible point.")
   else
+    # Barrier method starting from the stricly feasible point
     barrier_method(C, A, b, mu[1 : m])
   end
 end
 
+# Classic barrier method
 function barrier_method(C, A, b, mu_0)
   n = size(A, 2)
   mu = mu_0
@@ -42,12 +34,14 @@ function barrier_method(C, A, b, mu_0)
   (1 / t * inv(F(C, A, mu)), mu)
 end
 
+# Barrier method that also stops after finding a point with the last coordinate negative
+# This is used in order to find the stricly feasible point
 function barrier_method_stop(C, A, b, mu_0)
   m = size(A, 1)
   n = size(A, 2)
   mu = mu_0
   gamma = 15
-  t = 1
+  t = 10
   while true
     mu = newton_stop(t, C, A, b, mu)
     if n / t < eps || mu[m] < 0
@@ -58,6 +52,7 @@ function barrier_method_stop(C, A, b, mu_0)
   (1 / t * inv(F(C, A, mu)), mu)
 end
 
+# Classic Newton's method
 function newton(t, C, A, b, x_0)
   x = x_0
   while true
@@ -75,10 +70,11 @@ function newton(t, C, A, b, x_0)
   x
 end
 
+# Newton's method that also stops after finding a point with the last coordinate negative
+# This is used in order to find the stricly feasible point
 function newton_stop(t, C, A, b, x_0)
   m = size(A, 1)
   x = x_0
-  max_iter = 100
   iter = 0
   while true
     iter += 1
@@ -87,17 +83,13 @@ function newton_stop(t, C, A, b, x_0)
     inv_nabla2_f_t_x = inv(nabla2_f_t_x)
     delta_x = - inv_nabla2_f_t_x * nabla_f_t_x
     lambda2 = - (nabla_f_t_x' * delta_x)[1]
-    if lambda2 / 2 < eps || x[m] < 0 || iter > max_iter
+    if lambda2 / 2 < eps || x[m] < 0
       break
     end
     fact = line_search(t, C, A, b, x, delta_x)
     x += fact * delta_x
   end
-  if iter > max_iter
-    x_0
-  else
-    x
-  end
+  x
 end
 
 function line_search(t, C, A, b, x, delta_x)
@@ -119,7 +111,7 @@ function nabla2_f_t(t, C, A, b, mu)
   inv_F_mu = inv(F(C, A, mu))
   for i = 1 : m
     for j = 1 : m
-      h[i, j] = trace(A[i, :, :]' * inv_F_mu * A[j, :, :] * inv_F_mu)
+      h[i, j] = trace(inv_F_mu * A[i, :, :] * inv_F_mu * A[j, :, :])
     end
   end
   h
@@ -154,19 +146,49 @@ function F(C, A, mu)
   aux + C
 end
 
-
 # Make the Convex.jl module available
 using Convex
 
-# Generate random problem data
-n = 5
-#C = randn(n, n); A = randn(n, n); b = randn(1, 1)
-A1 = A[1,:,:]; A2 = A[2,:,:]; b1 = b[1]; b2 = b[2];
+# Generate random problem in dimension sz * sz with two equality constraints:
+# one assures the fact the problem is bounded
+# the other is random
+#sz = 3
+#C = randn(sz, sz)
+#A = zeros(2, sz, sz)
+#A[1, :, :] = diagm(1 : sz)
+#A[2, :, :] = randn(sz, sz)
+#b = [1; randn(1)]
+#x = Variable(sz, sz)
+#problem = minimize(trace(C * x), trace(A[1, :, :] * x) == b[1], trace(A[2, :, :] * x) == b[2], lambdamin(x) >= 0)
 
-# Create a (column vector) variable of size n x 1.
-x = Variable(3,3)
+# Generate random problem in dimension sz * sz with one equality constraint that
+# assures the fact that the problem is bounded
+sz = 3
+C = randn(sz, sz)
+A = zeros(1, sz, sz)
+A[1, :, :] = diagm(1 : sz)
+b = [1]
+x = Variable(sz, sz)
+problem = minimize(trace(C * x), trace(A[1, :, :] * x) == b[1], lambdamin(x) >= 0)
 
-problem = minimize(trace(C * x), trace(A1 * x) == b1, trace(A2 * x) == b2, lambdamin(x) >= 0)
+# First debugging example
+# Opt: 13.9
+#C = [[1 2 3]; [2 9 0]; [3 0 7]]
+#A = zeros(2, 3, 3)
+#A[1, :, :] = [[1, 0, 1]; [0, 3, 7]; [1, 7, 5]]
+#A[2, :, :] = [[0, 2, 8]; [2, 6, 0]; [8, 0, 4]]
+#b = [11; 19]
+#x = Variable(3, 3)
+#problem = minimize(trace(C * x), trace(A[1, :, :] * x) == b[1], trace(A[2, :, :] * x) == b[2], lambdamin(x) >= 0)
+
+# Second debugging example
+# Opt: 4
+#C = [[1 0 0 0]; [0 1 0 0]; [0 0 2 0];[0 0 0 2]]
+#A = zeros(1, 4, 4)
+#A[1, :, :] = [[1 0 0 0]; [0 1 0 0]; [0 0 0 0]; [0 0 0 0]]
+#b = [4]
+#x = Variable(4, 4)
+#problem = minimize(trace(C * x), trace(A[1, :, :] * x) == b[1], lambdamin(x) >= 0)
 
 # Solve the problem by calling solve!
 solve!(problem)
@@ -175,9 +197,16 @@ solve!(problem)
 problem.status # :Optimal, :Infeasible, :Unbounded etc.
 
 # Get the optimum value
-problem.optval
+print("Expected:\n")
+print(problem.optval)
+print("\n")
 
-#Ap = zeros(1,n,n)
-#Ap[1,:,:] = A;
-(x,mu) = sdp_solver(C,A,b)
-print(trace(C*x))
+(x, mu) = sdp_solver(C, A, b)
+print("Obtained:\n")
+print(trace(C * x))
+print("\n")
+# Rank 1 testing
+#print("Rank 1 solution:\n")
+#v = rank1_solution(C, A, x)
+#print(trace(C * v * v'))
+print("\n")
